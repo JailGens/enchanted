b91717243f41703b3780ed305f1c4e259799e814
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2022-11-28 19:55:17 +1100

    Add api

---
+++ enchanted-api/src/main/java/net/jailgens/enchanted/CommandGroup.java
@@ -0,0 +1,60 @@
+package net.jailgens.enchanted;
+
+import org.intellij.lang.annotations.Pattern;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Unmodifiable;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Represents an immutable group of subcommands.
+ * <p>
+ * This class represents the commands that are created via
+ * {@link CommandFactory#createCommand(Object)} and {@link CommandRegistry#registerCommand(Object)}.
+ *
+ * @author Sparky983
+ * @since 0.0.0
+ */
+public interface CommandGroup extends Subcommand {
+
+    /**
+     * Returns a list of this command group's default command's parameters.
+     *
+     * @return this command group's default command's parameters.
+     * @since 0.0
+     */
+    @Override
+    @NotNull @Unmodifiable List<? extends @NotNull CommandParameter> getParameters();
+
+    /**
+     * Gets a subcommand with a name that matches the specified label, or contains an alias that
+     * matches the specified label.
+     * <p>
+     * A command name, or alias matches the label as defined by
+     * {@link String#equalsIgnoreCase(String)}.
+     * <p>
+     * If the specified label does not match {@link Command#NAME_PATTERN}, {@code Optional.empty()}
+     * is returned.
+     *
+     * @param label the label.
+     * @return an optional containing the command that was found, or {@code Optional.empty()}.
+     * @throws NullPointerException if the label is {@code null}
+     * @since 0.0.0
+     */
+    @Contract(pure = true)
+    @NotNull Optional<? extends @NotNull Subcommand> getCommand(@Pattern(NAME_PATTERN) @NotNull String label);
+
+    /**
+     * Gets all the registered commands of this registry.
+     * <p>
+     * The returned list is an unmodifiable collection of subcommands.
+     *
+     * @return all the registered commands of this registry.
+     * @since 0.0.0
+     */
+    @Contract(pure = true)
+    @NotNull @Unmodifiable Collection<? extends @NotNull Subcommand> getRegisteredCommands();
+}
---
+++ enchanted-api/src/main/java/net/jailgens/enchanted/CommandParameter.java
@@ -0,0 +1,52 @@
+package net.jailgens.enchanted;
+
+import net.jailgens.enchanted.annotations.Join;
+import org.intellij.lang.annotations.Identifier;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+
+/**
+ * Represents a {@link Subcommand}'s parameter.
+ *
+ * @author Sparky983
+ * @since 0.0
+ */
+public interface CommandParameter {
+
+    /**
+     * Gets this parameter's name.
+     * <p>
+     * The returned name can be assumed to not contain the space (SP) character.
+     *
+     * @return this parameter's name.
+     * @since 0.0
+     */
+    @Contract(pure = true)
+    @Identifier
+    @NotNull String getName();
+
+    /**
+     * Returns whether this parameter is optional.
+     *
+     * @return whether this parameter is optional.
+     * @since 0.0
+     * @see net.jailgens.enchanted.annotations.Optional
+     */
+    @Contract(pure = true)
+    boolean isOptional();
+
+    /**
+     * Returns string that separates multiple arguments to this single parameter.
+     * <p>
+     * If this parameter does not join multiple arguments together, {@code Optional.empty()} is
+     * returned.
+     *
+     * @return an optional containing the string that separates the arguments for this parameter.
+     * @see Join
+     * @since 0.0
+     */
+    @Contract(pure = true)
+    @NotNull Optional<@NotNull String> getSeparator();
+}
