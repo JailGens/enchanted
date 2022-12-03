b9a8beef83456473d501b833ed009163e8158298
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2022-12-04 07:25:33 +1100

    make argument resolver use arguments

--- enchanted-api/src/main/java/net/jailgens/enchanted/ArgumentResolver.java
+++ enchanted-api/src/main/java/net/jailgens/enchanted/ArgumentResolver.java
@@ -16,21 +16,21 @@ import java.lang.annotation.Annotation;
 public interface ArgumentResolver<A extends @NotNull Annotation> {
 
     /**
-     * Resolves the specified argument into an object of type {@link T}.
+     * Resolves the specified arguments into an object of type {@link T}.
      *
      * @param parameter the command parameter.
      * @param annotation the annotation.
      * @param type the type of the object.
-     * @param argument the argument after it has been parsed with an {@link ArgumentParser}.
+     * @param arguments the arguments after they have been parsed with an {@link ArgumentParser}.
      * @return the resolved object.
-     * @throws NullPointerException if the command parameter, annotation, type or argument is
+     * @throws NullPointerException if the command parameter, annotation, type or arguments is
      * {@code null} (optional).
      * @since 0.1.0
      */
     <T extends @Nullable Object> /*@NotNull*/ T resolve(@NotNull CommandParameter<T> parameter,
                                                         @NotNull A annotation,
                                                         @NotNull Class<@NotNull T> type,
-                                                        @NotNull String argument);
+                                                        @NotNull Arguments arguments);
 
     // @NotNull is commented out because by Kotlin and JSpecify rules, the return value may contain
     // null if the <T> is @Nullable. But the IDE says this is equivalent to the JSpecify MINUS_NULL
