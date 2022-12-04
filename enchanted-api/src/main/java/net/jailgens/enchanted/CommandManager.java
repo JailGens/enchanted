a33a1c8ef0dd4b3f6ee9c8d8716f562071322d31
Sparky983 <87631423+Sparky983@users.noreply.github.com>
2022-12-04 21:14:50 +1100

    Plain command registration
    Closes #34

--- enchanted-api/src/main/java/net/jailgens/enchanted/CommandManager.java
+++ enchanted-api/src/main/java/net/jailgens/enchanted/CommandManager.java
@@ -1,5 +1,8 @@
 package net.jailgens.enchanted;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 /**
  * The main manager for all commands.
  *
@@ -12,4 +15,19 @@ public interface CommandManager extends CommandFactory,
         ArgumentParserRegistry,
         ArgumentResolverRegistry {
 
+    /**
+     * Registers the specified command to this registry.
+     *
+     * @param command the command.
+     * @return the created command as specified in {@link CommandFactory}.
+     * @throws IllegalArgumentException if the command failed validation.
+     * @throws IllegalStateException if the command is currently registered to this registry or
+     * another command has been registered with a name or aliases that conflict with the command.
+     * @throws NullPointerException if the command is {@code null}.
+     * @see #registerCommand(Command)
+     * @see #unregisterCommand(Command)
+     * @since 0.0.0
+     */
+    @Contract(value = "_ -> new", mutates = "this")
+    @NotNull CommandGroup registerCommand(@NotNull Object command);
 }
--- enchanted-api/src/main/java/net/jailgens/enchanted/CommandRegistry.java
+++ enchanted-api/src/main/java/net/jailgens/enchanted/CommandRegistry.java
@@ -21,22 +21,20 @@ public interface CommandRegistry {
     /**
      * Registers the specified command to this registry.
      *
-     * @param command the command to add.
-     * @return the created command as specified in {@link CommandFactory}.
-     * @throws IllegalArgumentException if the command failed validation.
+     * @param command the command.
      * @throws IllegalStateException if the command is currently registered to this registry or
      * another command has been registered with a name or aliases that conflict with the command.
      * @throws NullPointerException if the command is {@code null}.
      * @see #unregisterCommand(Command)
      * @since 0.0.0
      */
-    @Contract(value = "_ -> new", mutates = "this")
-    @NotNull CommandGroup registerCommand(@NotNull Object command);
+    @Contract(mutates = "this")
+    @NotNull void registerCommand(@NotNull Command command);
 
     /**
      * Removes the specified command from this registry.
      *
-     * @param command the command to remove.
+     * @param command the command.
      * @throws IllegalStateException if the command is not currently registered to this registry.
      * @throws NullPointerException if the command is {@code null}.
      * @see #registerCommand(Object)
